b9ff88cbedb7fa75e3ad294afdba4d144b775f83
Jason <jiaze.gao@outlook.com>
2025-12-05 22:08:41 -0600

    Update 12/5/2025

--- main.cpp
+++ main.cpp
@@ -2,24 +2,92 @@
 #include "Leftist_Heap.h"
 #include "Skew_Heap.h"
 #include <chrono>
+#include <random>
 
-void buildSkew(int size) {
-    Skew_Heap<int> skew;
-    for (int i = 0 ; i < size ; i++) {
-        skew.enqueue(i);
+std::mt19937 engine(std::random_device{}());
+std::uniform_int_distribution<int> RNG_SMALL(1, 9999);
+
+long long insertBuildSkew(const int size) {
+    // Generate random integers
+    std::vector<int> items(size, 0);
+    for (int i = 0; i < size; i++) {
+        items[i] = RNG_SMALL(engine);
     }
+
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Skew_Heap<int>::insertBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
 }
 
-void buildLeftist(int size) {
-    Leftist_Heap<int> leftist;
-    for (int i = 0 ; i < size ; i++) {
-        leftist.enqueue(i);
-    }
+long long insertBuildSkew(const std::vector<int> &items) {
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Skew_Heap<int>::insertBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
 }
 
+long long insertBuildLeftist(const int size) {
+    // Generate random integers
+    std::vector<int> items(size, 0);
+    for (int i = 0; i < size; i++) {
+        items[i] = RNG_SMALL(engine);
+    }
+
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Leftist_Heap<int>::insertBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
+
+long long insertBuildLeftist(const std::vector<int> &items) {
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Leftist_Heap<int>::insertBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
+
+long long quickBuildSkew(const int size) {
+    // Generate random integers
+    std::vector<int> items(size, 0);
+    for (int i = 0; i < size; i++) {
+        items[i] = RNG_SMALL(engine);
+    }
+
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Skew_Heap<int>::quickBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
+
+long long quickBuildSkew(const std::vector<int> &items) {
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Skew_Heap<int>::quickBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
+
+long long quickBuildLeftist(const int size) {
+    // Generate random integers
+    std::vector<int> items(size, 0);
+    for (int i = 0; i < size; i++) {
+        items[i] = RNG_SMALL(engine);
+    }
+
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Leftist_Heap<int>::quickBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
+
+long long quickBuildLeftist(const std::vector<int> &items) {
+    // Perform enqueues
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Leftist_Heap<int>::quickBuild(items);
+    return (std::chrono::high_resolution_clock::now() - start_time).count();
+}
 
 int main() {
-    Skew_Heap<int> heap;
 
     /*
     std::vector<int> h1 = {15,31,1,13,42,8,57,27,33};
@@ -34,18 +102,60 @@ int main() {
     heap1.printHeap();
     */
 
-    auto start_time = std::chrono::high_resolution_clock::now();
-    buildSkew(1000);
-    auto diff1 = std::chrono::high_resolution_clock::now() - start_time;
+    // Warm up
+    Skew_Heap<int> heap10;
+    for (int i = 0; i < 100000; i++) {
+        heap10.enqueue(i);
+    }
 
-    auto start_time2 = std::chrono::high_resolution_clock::now();
-    buildSkew(10000);
-    auto diff2 = std::chrono::high_resolution_clock::now() - start_time2;
+    std::vector<int> items(10000, 0);
 
-    std::cout << diff1.count() << std::endl << diff2.count() << std::endl;
+    // Sorted Insert
+    for (int i = 0; i < 10000; i++) {
+        items[0] = i;
+    }
+    std::cout << "Sorted Skew Insert: " << insertBuildSkew(items) << std::endl;
+    std::cout << "Sorted Leftist Insert: " << insertBuildLeftist(items) << std::endl;
+
+    // Reversed Insert
+    for (int i = 9999; i >= 0; i--) {
+        items[0] = i;
+    }
+    std::cout << "Reversed Skew Insert: " << insertBuildSkew(items) << std::endl;
+    std::cout << "Reversed Leftist Insert: " << insertBuildLeftist(items) << std::endl;
+
+    // Random Insert
+    for (int i = 0; i < 10000; i++) {
+        items[0] = RNG_SMALL(engine);
+    }
+    std::cout << "Random Skew Insert: " << insertBuildSkew(items) << std::endl;
+    std::cout << "Random Leftist Insert: " << insertBuildLeftist(items) << std::endl;
+
+    // Sorted Quick
+    for (int i = 0; i < 10000; i++) {
+        items[0] = i;
+    }
+    std::cout << "Sorted Skew Quick: " << quickBuildSkew(items) << std::endl;
+    std::cout << "Sorted Leftist Quick: " << quickBuildLeftist(items) << std::endl;
+
+    // Reversed Quick
+    for (int i = 9999; i >= 0; i--) {
+        items[0] = i;
+    }
+    std::cout << "Reversed Skew Quick: " << quickBuildSkew(items) << std::endl;
+    std::cout << "Reversed Leftist Quick: " << quickBuildLeftist(items) << std::endl;
+
+    // Random Quick
+    for (int i = 0; i < 10000; i++) {
+        items[0] = RNG_SMALL(engine);
+    }
+    std::cout << "Random Skew Quick: " << quickBuildSkew(items) << std::endl;
+    std::cout << "Random Leftist Quick: " << quickBuildLeftist(items) << std::endl;
+
+    Skew_Heap<int> heap;
 
     while (true) {
-        std::cout << "INPUT: ";
+        std::cout << "\nINPUT: ";
         int input;
         std::cin >> input;
 
